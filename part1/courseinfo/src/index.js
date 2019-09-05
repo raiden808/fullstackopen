df82e4c1305ba2f4490edabb87e4f1e0782827a4
raiden <raiden.nalasa@gmail.com>
2019-09-05 18:16:45 +0800

    part1 1,2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,8 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const Header = (props) => {
+	return(
+		<h1>{props.course}</h1>
+	)
+}
+
+const Content = (props) => {
+	return (
+		<div>
+			<Part />
+			<Part />
+			<Part />
+		</div>
+	)
+}
+
+const Part = (props) => {
+	return (
+		<p>
+			Test
+		</p>
+	)
+}
+
+
+
 const App = () => {
 	const course        = 'Half Stack application development'
+
+	const parts = 
+	{
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'tate of the component',
+                exercises: 14
+            }
+        ]
+    };
+
+    // access single array
+    console.log(parts.parts[0].name)
+
 	const part1         = 'Fundamentals of React'
 	const excercises1   = 10
 	const part2         = 'Using props to pass data'
@@ -12,7 +61,8 @@ const App = () => {
 
 	return (
 		<div>
-			<h1>{course}</h1>
+			<Header course={course} />
+			<Content />
 			<p>
 				{part1} {excercises1}
 			</p>
