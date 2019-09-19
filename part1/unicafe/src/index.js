d6615335eadc2f47846a807c2ea58531012ccdeb
raiden808 <raiden.nalasa@gmail.com>
2019-09-20 06:08:05 +0800

    implement hook and handleclick

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
+const App = () =>{
+	//save clicks of each button to own state
+	const [good,setGood] = useState(0);
+	const [neutral,setNeutral] = useState(0);
+	const [bad,setBad] = useState(0);
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+	const handleClick = (rating) =>{
+		switch (rating) {
+			case "good":
+				setGood(good+1)
+				break;
+			case "neutral":
+				setNeutral(neutral+1)
+				break;
+			case "bad":
+				setBad(bad+1)
+				break;
+			default:
+				// statements_def
+				break;
+		}
+	}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+	return (
+		<div>
+			<h2>give feedback</h2>
 
+			<button onClick={()=>{handleClick("good")}} >good</button>
+			<button onClick={()=>{handleClick("neutral")}} >neutral</button>
+			<button onClick={()=>{handleClick("bad")}} >bad</button>
+
+			<h2>Statistics</h2>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
