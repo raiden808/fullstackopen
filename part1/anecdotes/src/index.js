a2187876e7077809f6a43b53804005f88e6cbe5e
raiden808 <raiden.nalasa@gmail.com>
2019-09-27 05:35:57 +0800

    add vote button

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -5,17 +5,24 @@ import './index.css';
 const App = (props) =>{
 	const [selected,setSelected] = useState(0);
 
-  const handleClick = () =>{
+  //set state random generated number
+  const handleClickRandom = () =>{
+   
     setSelected(Math.round(Math.floor(Math.random() * props.anecdotes.length)))
 
     console.log(selected)
+  }
+
+  const handleClickVote = () =>{
+
   }
 
 	return(
 		<div>
       {props.anecdotes[selected]}
       <div>
-        <button onClick={handleClick}>next anecdote</button>
+        <button onClick={handleClickVote}>vote</button>
+        <button onClick={handleClickRandom}>next anecdote</button>
       </div>
     </div>
 	);
@@ -34,7 +41,3 @@ ReactDOM.render(
   <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
