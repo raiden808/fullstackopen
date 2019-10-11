013935451d0afa38a073d09d92f1ddd409e4c1b9
raiden808 <raiden.nalasa@gmail.com>
2019-10-11 08:26:53 +0800

    Use reduce on total

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,50 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// const Header = ({props}) => {
-//     return(
-//         <h1>{props.course}</h1>
-//     )
-// }
-
-
-
-// const Content = (props) => {
-// 	//console.log(props)
-//     return (
-//         <div>
-//             <Part part={props.parts[0]} />
-//             <Part part={props.parts[1]} />
-//             <Part part={props.parts[2]} />
-//         </div>
-//     )
-// }
-
-// const Part = (props) => {
-//     return (
-//         <p>
-//             {props.part.name} {props.part.exercises}
-//         </p>
-//     )
-// }
-
-// const Total = (props) => {
-
-//     let partCount = 0;
-//      props.parts.forEach( function(item, index) {
-//          partCount += item.exercises;
-//      });
-
-//     return(
-//         <p>Number of excercises {partCount}</p>
-//     )
-// }
-
-/*
-* Experimental Content
-*/
-
-
 const Header = ({courseName}) => {
     return(
         <h1>{courseName}</h1>
@@ -53,25 +9,22 @@ const Header = ({courseName}) => {
 }
 
 const Parts = ({parts}) => {
+  const theParts = parts.map((part, index) => {
+    //console.log(part)
+     return(
+       <>
+          <p>
+           {part.name} {part.exercises}
+          </p>
+       </>
+     )
+  })
 
-    //console.log(parts)
-
-    const theParts = parts.map((part, index) => {
-      //console.log(part)
-       return(
-         <>
-            <p>
-             {part.name} {part.exercises}
-            </p>
-         </>
-       )
-    })
-
-    return(
-        <>
-          {theParts}
-        </>
-    )
+  return(
+      <>
+        {theParts}
+      </>
+  )
 }
 
 const Content = ({courses}) =>{
@@ -95,30 +48,22 @@ const Content = ({courses}) =>{
 
 const Total = ({parts}) =>{
 
-  //gather all array here.
-  const theTotal = parts.map((part, index) => {
-
-    const excercise = [];
-    excercise.push(part.exercises);
-
-    return excercise
-  })
-
-  console.log(theTotal);
+  const totalExercise = [];
+  parts.forEach(function (item, index) {
+    //console.log(item, index);
+    totalExercise.push(item.exercises)
+  });
 
   return(
-    <>
-      Total {}
-    </>
+    <p>
+      Total {totalExercise.reduce((a, b) => a + b)}
+    </p>
   )
 }
 
 const Course = ({course}) => {
     return (
         <div>
-            {/*<Header course={course.name} />*/}
-            {/*<Content parts={course.parts} />*/}
-            {/*  <Total parts={course.parts} />*/}
             <Content courses={course} />
         </div>
     )
