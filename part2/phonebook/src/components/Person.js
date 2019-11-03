98169fb5464233759642442fbc7812855337267a
raiden808 <raiden.nalasa@gmail.com>
2019-11-04 07:48:06 +0800

    remove comments

--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -4,21 +4,11 @@ import React from "react";
 const Person = ({ person, search }) => {
 
 	console.log(person.name.indexOf(search), person.name)
-
 	let nameValue = person.name.toLowerCase();
-
-	// if(person.name.indexOf(search) >= 1){
-	// 	return <li>{person.name} {person.phone}</li>;
-	// }
-
-	// else{
-	// 	return <li>{person.name} {person.phone}</li>;
-	// }
 
 	if(search == ""){
 		return <li>{person.name} {person.phone}</li>
 	}
-
 
 	else{
 		if(nameValue.indexOf(search) >= 0){
@@ -29,15 +19,6 @@ const Person = ({ person, search }) => {
 			return <></>
 		}
 	}
-
-	// if(search == "test"){
-	// 	return "test";
-	// }
-
-	// else{
-	// 	return <li>{person.name} {person.phone}</li>;
-	// }
-	
 };
 
 export default Person;
