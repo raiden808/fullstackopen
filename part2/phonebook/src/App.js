6718bf638cfaca0265a857e5f01fd324e38b74ed
raiden808 <raiden.nalasa@gmail.com>
2019-11-15 05:23:13 +0800

    added json db and server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState} from 'react';
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
+
+import axios from 'axios'
 
 import Person from "./components/Person";
 import Filter from "./components/Filter";
@@ -8,17 +10,20 @@ import PersonForm from "./components/PersonForm";
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-    { name: 'Mary Calvin', number: '39-23-6422222' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName]    = useState('')
   const [newPhone, setNewPhone]  = useState('')
   const [search,setSearch]       = useState('')
+
+  /*retrieve json object*/
+  useEffect(()=>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response =>{
+        setPersons(response.data)
+      })
+  },[])
 
   /*
   * Name handler
