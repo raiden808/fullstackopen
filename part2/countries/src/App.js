044840559889716761486396e5cd335dcc855c2c
raiden808 <raiden.nalasa@gmail.com>
2019-11-19 05:49:21 +0800

    Update App.js

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React,{ useState, useEffect} from 'react';
+import axios from 'axios'
 
 //hook example
 // const hook = () => {
@@ -17,13 +17,41 @@ import React from 'react';
 
 function App() {
 
+	const [newCountry, setNewCountry] = useState('');
+
+	const handleInputChange = e =>{
+		const inputText = e.target.value;
+
+		//setNewCountry(inputText);
+
+		//useEffect(hook(inputText), [])
+	}
+
+	//hook example
+	const hook = (s) => {
+	  console.log('effect')
+	  axios
+	    .get('https://restcountries.eu/rest/v2/name/{s}')
+	    .then(response => {
+	      console.log('promise fulfilled')
+	    })
+	}
+
   
-  
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+  	return (
+	    <div className="App">
+	      <form>
+	      	<div>
+	      		find countries 
+	      		<input 
+	      			value={newCountry}
+	      			type="text"  
+	      			onChange={handleInputChange}
+	      		/>
+	      	</div>
+	      </form>
+	    </div>
+  	);
 }
 
 export default App;
