e706974e0ef3b2d9bbac4718fbf47484beea8d35
raiden808 <raiden.nalasa@gmail.com>
2019-11-18 09:21:57 +0800

    axios isntall

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
+
+//hook example
+// const hook = () => {
+//   console.log('effect')
+//   axios
+//     .get('http://localhost:3001/notes')
+//     .then(response => {
+//       console.log('promise fulfilled')
+//       setNotes(response.data)
+//     })
+// }
+
+// useEffect(hook, [])
+
+
 function App() {
+
+  
+  
   return (
     <div className="App">
       <h1>Hello</h1>
