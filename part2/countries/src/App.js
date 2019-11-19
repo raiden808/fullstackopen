8073d562dae7d7203a03ba98def9fc3c9c84592a
raiden808 <raiden.nalasa@gmail.com>
2019-11-19 12:06:34 +0800

    infinite loop refactor

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,44 +1,63 @@
 import React,{ useState, useEffect} from 'react';
 import axios from 'axios'
 
-//hook example
-// const hook = () => {
-//   console.log('effect')
-//   axios
-//     .get('http://localhost:3001/notes')
-//     .then(response => {
-//       console.log('promise fulfilled')
-//       setNotes(response.data)
-//     })
-// }
-
-// useEffect(hook, [])
-
-
 function App() {
 
 	const [newText, setText] = useState('');
+	//const [countries, setCountries] = useState([])
+	const [output, setOutput] = useState();
+
+	let renderOutput;
 
 	const handleInputChange = e =>{
 		const inputText = e.target.value;
 
-		setNewCountry(inputText);
+		setText(inputText);
 	}
 
-	useEffect(() => {
-	  console.log('effect')
 
-	  if(newText != ""){
-	  	axios
-	    .get(`https://restcountries.eu/rest/v2/name/${newText}`)
-	    .then(response => {
-	      console.log('promise fulfilled')
-	      console.log(response.data)
-	      // setNotes(response.data)
-	    })
-	  }
-	 
-	})
+	/*
+	* By setting `newText` as a checker to only trigger if it's changed.
+	*/
+	useEffect(() => {
+	  	console.log('effect')
+	  	if(newText != ""){
+	  		axios
+		    .get(`https://restcountries.eu/rest/v2/name/${newText}`)
+		    .then(response => {
+
+				console.log(response.data)
+
+	      		if(Object.keys(response.data).length > 10){
+	      			setOutput("Too many matches, specify another filter.");
+	      		}
+
+	      		else if(Object.keys(response.data).length <= 5){
+
+	      			let filteredCountry = [];
+	      			const countries = response.data.map(country=>{
+
+	      				filteredCountry.push(<li key={country.alpha2Code}>{country.name}</li>)
+	      			})
+	      			return setOutput(filteredCountry)
+	      		}
+
+	      		else if(Object.keys(response.data).length == 1){
+
+
+
+	      			let filteredCountry = [];
+	      			const countries = response.data.map(country=>{
+
+	      				console.log(country)
+
+	      				filteredCountry.push(<li key={country.alpha2Code}>{country.name}</li>)
+	      			})
+	      			return setOutput(filteredCountry)
+	      		}
+	    	})
+	  	}
+	},[newText])
 
   
   	return (
@@ -47,12 +66,15 @@ function App() {
 	      	<div>
 	      		find countries 
 	      		<input 
-	      			value={newCountry}
+	      			value={newText}
 	      			type="text"  
 	      			onChange={handleInputChange}
 	      		/>
 	      	</div>
 	      </form>
+	      <div>
+	     	{output}
+	      </div>
 	    </div>
   	);
 }
